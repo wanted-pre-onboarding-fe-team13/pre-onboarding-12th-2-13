71c807d542ae17e2fbac2fbbe7111e172698a622
Patata <patattta93@gmail.com>
2023-08-31 15:59:10 +0900

    feat: api 추가, response type 지정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -2,4 +2,17 @@ import { Octokit } from 'octokit';
 
 export const octokit = new Octokit({
   auth: import.meta.env.VITE_GITHUB_TOKEN,
+  baseUrl: import.meta.env.VITE_BASE_API_URL,
 });
+
+export const getIssuesPage = async (pageParam = 1) => {
+  const { data } = await octokit.request(`GET /issues?state=open&sort=comments&page=${pageParam}`);
+  return data;
+};
+
+export const getIssueDetails = async (id: number) => {
+  if (id) {
+    const { data } = await octokit.request(`GET /issues/${id}`);
+    return data;
+  }
+};
